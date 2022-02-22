00a8a93a7bf2d9ed1421fcc86cef67e48f6da0a4
Micaella Morales <morales.micaella@gmail.com>
2022-02-21 19:23:31 -0800

    Initial Commit

---
+++ Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Micaella Morales on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Micaella Morales on 2/21/22.
+//
+
+import SwiftUI
+
+// Color
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
